bf90402cfc36dd13d06dd774a7f01925dce1851d
sadıkahmet <sadikahmetozdemir@outlook.com>
2022-06-01 10:41:23 +0300

    create-screens

---
+++ app/src/main/java/com/sadikahmetozdemir/rainy/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.sadikahmetozdemir.rainy.utils
+
+object Constants {
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5/weather/"
+    const val API_KEY = "dc1a1c64526aa082370123c7e1dcf8a2"
+}
